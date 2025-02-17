55eab044c8cda1418605e6c392c6522181f52ab1
Parshnatvardhna <parshantvardhan8@gmail.com>
2025-02-17 18:26:52 +0530

    React baiscs 1

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+
+const parent= React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"H1 tag is here"),
+        React.createElement("h2",{},"H2 tag is here"),
+    ])
+
+ );
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
