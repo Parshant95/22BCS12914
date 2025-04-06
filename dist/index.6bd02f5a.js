eccfc4406f855465586a202dd0fc6ffadecb4f1f
Parshnatvardhna <parshantvardhan8@gmail.com>
2025-04-07 00:37:54 +0530

    commit

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "H1 tag is here"),
+    React.createElement("h2", {}, "H2 tag is here")
+]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
