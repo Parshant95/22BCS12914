c1d63e2bd0d580774a97dac961dd2d4d9e2eeca9
Parshnatvardhna <parshantvardhan8@gmail.com>
2025-04-07 13:33:17 +0530

    pushing file

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent= React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
@@ -8,7 +8,7 @@ const parent= React.createElement("div",{id:"parent"},
     ])
  
  );
-
+console.log(parent);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
